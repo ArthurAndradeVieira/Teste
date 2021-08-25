9bb7f710e18a7f00572c5f2a205c0372340c5637
ArthurAndradeVieira <89277379+ArthurAndradeVieira@users.noreply.github.com>
2021-08-25 11:09:30 -0300

    mudanca cor azul

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default props => {
 
 const screenOptions = {
   headerStyle: {
-    backgroundColor : '#f4511e'
+    backgroundColor : 'blue'
   },
   headerTintColor : '#fff',
   headerTitleSytle : {
